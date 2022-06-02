50c02fe408e482d0ed95d6b8fc4e97f4f2cefc79
BenjaminBrdl <106767922+BenjaminBrdl@users.noreply.github.com>
2022-06-02 22:48:29 +0200

    add commit

--- RobotPREVERT/src/main/include/RobotContainer.h
+++ RobotPREVERT/src/main/include/RobotContainer.h
@@ -1,7 +1,22 @@
 #pragma once
 
-#include "subsystems/Drivetrain.h"
+#include <frc2/command/Command.h>
 #include <frc/Joystick.h>
-#include <frc/smartdashboard/SmartDashboard.h>
+#include <frc2/command/button/JoystickButton.h>
 
 #include "subsystems/Drivetrain.h"
+
+#include "commands/Drive.h"
+
+class RobotContainer
+{
+public:
+    RobotContainer();
+    Drivetrain m_Drivetrain;
+
+private:
+    void ConfigureButtonBindings();$
+
+    frc::Joystick m_DriverRightJoystick{DRIVER_JOYSTICK_RIGHT_ID};
+    frc::Joystick m_DriverLeftJoystick{DRIVER_JOYSTICK_LEFT_ID};
+};
