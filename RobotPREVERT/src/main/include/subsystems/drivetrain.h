a0e7638b32fbe2b9f296822ee93f38e97d3c49d4
mimi980 <90112270+mimi980@users.noreply.github.com>
2022-06-17 13:41:35 +0200

    add talonsrx

--- RobotPREVERT/src/main/include/subsystems/drivetrain.h
+++ RobotPREVERT/src/main/include/subsystems/drivetrain.h
@@ -5,7 +5,7 @@
 #pragma once
 
 #include "Constants.h"
-#include <ctre/phoenix/motorcontrol/can/VictorSPX.h>
+#include <ctre/phoenix/motorcontrol/can/TalonSRX.h>
 #include <frc2/command/SubsystemBase.h>
 
 class Drivetrain : public frc2::SubsystemBase
@@ -14,12 +14,11 @@ public:
   Drivetrain();
   void Drive(float forward, float turn);
 
-
 private:
-  ctre::phoenix::motorcontrol::can::VictorSPX m_MotorRight{CAN_ID_DRIVETRAIN_MOTOR_RIGHT};
-  ctre::phoenix::motorcontrol::can::VictorSPX m_MotorRightFollow{CAN_ID_DRIVETRAIN_MOTOR_RIGHT_FOLLOW};
-  ctre::phoenix::motorcontrol::can::VictorSPX	m_MotorLeft{CAN_ID_DRIVETRAIN_MOTOR_LEFT};
-  ctre::phoenix::motorcontrol::can::VictorSPX	m_MotorLeftFollow{CAN_ID_DRIVETRAIN_MOTOR_LEFT_FOLLOW};
+  ctre::phoenix::motorcontrol::can::TalonSRX m_MotorRight{CAN_ID_DRIVETRAIN_MOTOR_RIGHT};
+  ctre::phoenix::motorcontrol::can::TalonSRX m_MotorRightFollow{CAN_ID_DRIVETRAIN_MOTOR_RIGHT_FOLLOW};
+  ctre::phoenix::motorcontrol::can::TalonSRX m_MotorLeft{CAN_ID_DRIVETRAIN_MOTOR_LEFT};
+  ctre::phoenix::motorcontrol::can::TalonSRX m_MotorLeftFollow{CAN_ID_DRIVETRAIN_MOTOR_LEFT_FOLLOW};
 
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
